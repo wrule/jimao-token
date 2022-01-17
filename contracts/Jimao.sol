ba63b63df39049a806646a8fd4f8c06807ea2b4c
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-17 15:55:39 +0800

    铸币

--- contracts/Jimao.sol
+++ contracts/Jimao.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Jimao is ERC20 {
   constructor()
   ERC20("JimaoToken", "JIMAO") {
-    _mint(msg.sender, 21000000 * 1e18);
+    _mint(msg.sender, 21000000 * (10^decimals()));
   }
 }
