dec464232c585f133c5d837a5ef97a5480a200b3
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-24 00:57:48 +0800

    连接器应用

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import 'antd/dist/antd.min.css';
 import zhCN from 'antd/es/locale/zh_CN';
 import { Button, ConfigProvider, Layout } from 'antd';
 import { Content, Header } from 'antd/lib/layout/layout';
+import { Connector } from './components/connector';
 import style from './App.module.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Layout className={style.layout}>
         <Header className={style.header}>
           <h1 className={style.title}>Jimao Token</h1>
+          <Connector />
         </Header>
         <Content className={style.content}>
           <Button>你好，世界</Button>
