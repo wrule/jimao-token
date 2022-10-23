9612b077571aedc3075bc8672689453e9582fd28
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-24 00:28:55 +0800

    默认页面

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,18 @@
 import 'antd/dist/antd.min.css';
-import { Button } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import { Button, ConfigProvider, Layout } from 'antd';
+import { Content, Header } from 'antd/lib/layout/layout';
 
 function App() {
   return (
-    <div className="App">
-      <Button>你好，世界</Button>
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <Layout>
+        <Header>你好，世界</Header>
+        <Content>
+          <Button>你好，世界</Button>
+        </Content>
+      </Layout>
+    </ConfigProvider>
   );
 }
 
