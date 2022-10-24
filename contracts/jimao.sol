b32d2996b39ff657af5a40ecee482ad308307ddb
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-24 17:15:47 +0800

    两个erc20敞亮

--- contracts/jimao.sol
+++ contracts/jimao.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract JIMAO is ERC20 {
   constructor()
@@ -9,8 +10,21 @@ contract JIMAO is ERC20 {
     _mint(address(this), 1e18 * 1e18);
   }
 
+  IERC20 private constant USDC = IERC20(address(0xaE0c2a01e400324Cc946074a1Aef5E5eC1Fa9671));
+  IERC20 private constant USDM = IERC20(address(0xC9120aE102660b1Ec392Db9C4D293dADC89a47C1));
+
   function airdrop()
   public {
     this.transfer(msg.sender, 1e18 * 100);
+  }
+
+  function usdc_to_usdm()
+  public {
+
+  }
+
+  function usdm_to_usdc()
+  public {
+
   }
 }
