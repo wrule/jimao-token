da5beafb1e6eef2e0592ff7b2969df4519d46e19
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-24 14:33:44 +0800

    代币归合约

--- contracts/jimao.sol
+++ contracts/jimao.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract JIMAO is ERC20 {
   constructor()
   ERC20("zkSync Jimao coin", "JIMAO") {
-    _mint(msg.sender, 1e18 * 1e18);
+    _mint(address(this), 1e18 * 1e18);
   }
 }
