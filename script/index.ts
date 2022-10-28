477d025ca96c559f7487efdf0f1bd65a1734ebdf
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-28 14:41:01 +0800

    提交代码

--- script/index.ts
+++ script/index.ts
@@ -8,13 +8,14 @@ async function main() {
   const jimao = new JIMAO(wallet.provider, wallet);
   // const balance = await jimao.balanceOf('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
   // console.log(ethers.utils.formatEther(balance));
-  console.log('发送交易');
-  const tx = await jimao.airdrop();
-  console.log('等待确认');
-  await tx.wait();
-  console.log('查询余额');
-  const balance = await jimao.balanceOf(wallet.address);
-  console.log(balance.toString());
+
+  // console.log('发送交易');
+  // const tx = await jimao.airdrop();
+  // console.log('等待确认');
+  // await tx.wait();
+  // console.log('查询余额');
+  // const balance = await jimao.balanceOf(wallet.address);
+  // console.log(balance.toString());
 }
 
 main();
