49f646fa3c3b3aa2c300d60f0b9f02d3d96bdff4
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-28 15:12:43 +0800

    wash

--- script/wash.ts
+++ script/wash.ts
@@ -1,13 +1,19 @@
+import { ethers } from 'ethers';
 
 interface wallet {
   address: string;
   pri_key: string;
 }
 
+function create_wallet(num: number) {
+  return Array(num).fill(0).map(() => ethers.Wallet.createRandom());
+}
+
 function wash_eth(
   from: string,
-  hop_count: number,
   to: string,
+  amount: number,
+  hop_count: number,
 ) {
 
 }
