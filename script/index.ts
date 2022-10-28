6eb840ad9e81eb82cd03b7c8f45dbc7b1c36fed8
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-28 12:11:25 +0800

    尝试编写自动化脚本

--- script/index.ts
+++ script/index.ts
@@ -1,2 +1,10 @@
+import { ethers } from 'ethers';
+const secret = require('../.secret.json');
 
-console.log('你好，世界');
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${secret.prj_id}`);
+  const balance = await provider.getBalance('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
+  console.log(ethers.utils.formatEther(balance));
+}
+
+main();
