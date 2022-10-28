a1a804f7852298affb3232b06d8c8875107e6bdc
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-28 14:39:14 +0800

    提交代码

--- script/index.ts
+++ script/index.ts
@@ -4,11 +4,19 @@ const secret = require('../.secret.json');
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${secret.prj_id}`);
-  const balance = await provider.getBalance('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
-  console.log(ethers.utils.formatEther(balance));
-  const jimao = new JIMAO(provider, provider.getSigner());
-  const b = await jimao.balanceOf('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
-  console.log(ethers.utils.formatEther(b));
+  const wallet = new ethers.Wallet(secret.pri_key, provider);
+  const jimao = new JIMAO(wallet.provider, wallet);
+  // const balance = await jimao.balanceOf('0x28dF8c4d5fc59cA685546e817772181Fb717E503');
+  // console.log(ethers.utils.formatEther(balance));
+  while (true) {
+    console.log('发送交易');
+    const tx = await jimao.airdrop();
+    console.log('等待确认');
+    await tx.wait();
+    console.log('查询余额');
+    const balance = await jimao.balanceOf(wallet.address);
+    console.log(balance.toString());
+  }
 }
 
 main();
