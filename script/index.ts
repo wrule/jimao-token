56c1304e825f757d6a8e6c1473fe3faeebd94128
jimao <jimao@jimaodeMBP.lan>
2022-10-29 13:19:24 +0800

    提交代码

--- script/index.ts
+++ script/index.ts
@@ -17,16 +17,40 @@ async function main() {
   // const balance = await jimao.balanceOf(wallet.address);
   // console.log(balance.toString());
 
-  while (true) {
-    console.log('发送...');
-    const tx = await wallet.sendTransaction({
-      to: '0x28dF8c4d5fc59cA685546e817772181Fb717E503',
-      value: ethers.utils.parseEther('0.1'),
-    });
-    console.log('确认...');
-    await tx.wait();
-    console.log('完成');
-  }
+  // while (true) {
+
+  const data = await wallet.getFeeData();
+  console.log('gasPrice', data.gasPrice?.toString());
+  console.log('lastBaseFeePerGas', data.lastBaseFeePerGas?.toString());
+  console.log('maxFeePerGas', data.maxFeePerGas?.toString());
+  console.log('maxPriorityFeePerGas', data.maxPriorityFeePerGas?.toString());
+
+  // return;
+
+  const target = '0x28dF8c4d5fc59cA685546e817772181Fb717E503';
+
+  let balance = await wallet.getBalance();
+  console.log('余额', balance.toString());
+
+  console.log('发送...');
+  const tx = await wallet.sendTransaction({
+    to: target,
+    value: ethers.utils.parseEther('0.106'),
+  });
+  console.log('确认...');
+  await tx.wait();
+  console.log('完成', tx.hash);
+  console.log(tx);
+  console.log('value', tx.value.toString());
+  console.log('gasLimit', tx.gasLimit.toString());
+  console.log('maxFeePerGas', tx.maxFeePerGas?.toString());
+  console.log('maxPriorityFeePerGas', tx.maxPriorityFeePerGas?.toString());
+
+  balance = await wallet.getBalance();
+  console.log('余额', balance.toString());
+
+
+  // }
 
   // while (true) {
   //   const rwallet = ethers.Wallet.createRandom();
