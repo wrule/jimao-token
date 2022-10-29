6192e0b9658dbc680911dc0142b1afe5cf15852c
jimao <jimao@jimaodeMBP.lan>
2022-10-29 19:29:05 +0800

    提交代码

--- script/index.ts
+++ script/index.ts
@@ -49,7 +49,7 @@ async function main() {
   const provider = new ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${secret.prj_id}`);
   const wallet = new ethers.Wallet(secret.pri_key, provider);
   const target = '0x28dF8c4d5fc59cA685546e817772181Fb717E503';
-  await wash_eth(wallet, target, 10);
+  await wash_eth(wallet, target, 5);
   return;
   // const data = await wallet.getFeeData();
   // console.log('gasPrice', data.gasPrice?.toString());
